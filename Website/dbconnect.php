fb29183a77582be86608cb4ff627d50ded7a3f50
Zekun Tang <tangzhuangkun@gmail.com>
2016-08-10 16:28:25 -0700

    Update dbconnect.php
    Delete server access info

--- Website/dbconnect.php
+++ Website/dbconnect.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting( E_ALL & ~E_DEPRECATED & ~E_NOTICE );
-if(!mysql_connect("ix.cs.uoregon.edu:3366","hotel","hotel"))
+if(!mysql_connect("","",""))
 {
 	die('oops connection problem ! --> '.mysql_error());
 }
-if(!mysql_select_db("Hotel_Data"))
+if(!mysql_select_db(""))
 {
 	die('oops database selection problem ! --> '.mysql_error());
 }
